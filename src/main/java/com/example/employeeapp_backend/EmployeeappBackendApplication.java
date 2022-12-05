7b88c50e468c4ec4742f5ca4ebcb2904c35a154b
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-05 15:34:52 +0530

    first commit

---
+++ src/main/java/com/example/employeeapp_backend/EmployeeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeeapp_backend/EmployeeappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
