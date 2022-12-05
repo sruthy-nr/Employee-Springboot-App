93a2557c04c18613438be8e72a03b28b36f34b5b
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-05 15:46:10 +0530

    home commit

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @GetMapping("/")
     public String Homepage(){
-        return "Add employee";
+        return "Welcome to my website";
     }
     @GetMapping("/add")
     public String AddEmployee(){
