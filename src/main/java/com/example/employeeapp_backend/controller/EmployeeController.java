a0e3d88c595c2f06239e3d02949b1b69efb05650
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-06 10:30:03 +0530

    addreadvalues

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -15,7 +15,15 @@ public class EmployeeController {
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
     public String AddEmployee(@RequestBody Employees e){
 
+        System.out.println(e.getEmployeecode().toString());
         System.out.println(e.getEmployeename().toString());
+        System.out.println(e.getDesignation().toString());
+        System.out.println(e.getSalary());
+        System.out.println(e.getCompanyname().toString());
+        System.out.println(e.getMobile().toString());
+        System.out.println(e.getUsername().toString());
+        System.out.println(e.getPassword().toString());
+
         return "Employee added successfully";
     }
     @GetMapping("/view")
